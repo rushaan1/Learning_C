7babe1e29ce089d2f2860eb13807ebdd0f76338b
rushaan1 <cocrushaan3@gmail.com>
2023-09-09 17:29:41 +0530

    Updated arrays.c

--- Learning_C/arrays.c
+++ Learning_C/arrays.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 
-void arrayReverse() {
-	int _array[] = { 1,2,3,4,5,6,7,8,9,10 };
+void arrayReverse_and_print(int _array[], int n) {
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < n/2; i++) {
 
-		int last = _array[10 - i - 1];
+		int last = _array[n - i - 1];
 		int first = _array[i];
 
 		_array[i] = last;
-		_array[10 - i - 1] = first;
+		_array[n - i - 1] = first;
 	}
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < n; i++) {
 		printf("%d\n", _array[i]);
 	}
 }
 
-int main() {
-	return 0;
+void storeTables(int arr[][10], int n, int number) {
+	for (int i = 1; i <= 10; i++) {
+		arr[n][i] = number * i;
+	}
+
 }
 
+int main() {
+	int _n_array[] = {1,2,3,4,5,6,7,8};
+	//arrayReverse_and_print(_n_array, 8);
+
+	int _2dArray[2][10];
+	storeTables(_2dArray, 0, 2);
+	storeTables(_2dArray, 1, 3);
+
+
+	for (int i = 1; i <= 10; i++) {
+		printf("%d\n",_2dArray[0][i]);
+	}
+	printf("\n\n");
+	for (int i = 1; i <= 10; i++) {
+		printf("%d\n", _2dArray[1][i]);
+	}
+	return 0;
+}
